0ffe164f5f569bed36c93c0434a4bcc94a27a873
Liubov Kvasova <71709944+LiubovKvasova@users.noreply.github.com>
2022-06-21 20:54:39 +0200

    Implemented tests for handler.go

---
+++ handler_test.go
@@ -0,0 +1,57 @@
+package lab2
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrefixToPostfix(test *testing.T) {
+	expression := "+ 2 * 3 6"
+	expected := "2 3 6 * +"
+	input := strings.NewReader(expression)
+	output := bytes.NewBufferString("")
+
+	handler := &ComputeHandler{
+		Input: input,
+		Output: output,
+	}
+
+	err := handler.Compute()
+	if assert.Nil(test, err) {
+		assert.Equal(test, expected, output.String())
+	}
+}
+
+func TestEmptyExpression(test *testing.T) {
+    expression := ""
+    expected := ""
+    input := strings.NewReader(expression)
+	output := bytes.NewBufferString("")
+
+	handler := &ComputeHandler{
+		Input: input,
+		Output: output,
+	}
+
+	err := handler.Compute()
+	if assert.Nil(test, err) {
+		assert.Equal(test, expected, output.String())
+	}
+}
+
+func TestWrongExpression(test *testing.T) {
+	expression := ""
+    input := strings.NewReader(expression)
+	output := bytes.NewBufferString("")
+
+	handler := &ComputeHandler{
+		Input:  input,
+		Output: output,
+	}
+
+	err := handler.Compute()
+	assert.NotNil(test, err)
+}
