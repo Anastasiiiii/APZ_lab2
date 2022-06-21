6820a335a055567a0ed5338e849edcce71415ef2
Liubov Kvasova <71709944+LiubovKvasova@users.noreply.github.com>
2022-06-21 20:43:53 +0200

    Implemented handler.go

--- handler.go
+++ handler.go
@@ -1,12 +1,32 @@
 package lab2
 
+import (
+	"io"
+	"io/ioutil"
+)
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	expression, err := ioutil.ReadAll(ch.Reader)
+	if err != nil {
+		return err	
+	}
+	
+	result, err := PrefixToPostfix(string(prefix))
+	if err != nil {
+		return err	
+	}
+	
+	_, err := ch.Output.Write([]byte(result))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
